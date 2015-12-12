1b782fc108adf64a4507572457e74ce100002a6c
Psyringe <psyringe@protonmail.com>
2015-12-12 22:44:59 +0330

    Server now uses added packages as middlewares.

--- config/express.js
+++ config/express.js
@@ -1,7 +1,25 @@
  var express = require('express');
+ var morgan = require('morgan');
+ var bodyParser = require('body-parser');
+ var compress = require('compression');
+ var methodOverride = require('method-override');
 
  module.exports = function(){
     var app = express();
+
+    if(process.env.NODE_ENV === 'development'){
+        app.use(morgan('dev'));
+    }
+    else if(process.env.NODE_ENV === 'production'){
+        app.use(compress());
+    }
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+
     require('../app/routes/index.server.routes')(app);
     return app;
  };
