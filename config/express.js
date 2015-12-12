95627a63d95e9d84ce2f1bba7f3647317a28539d
Psyringe <psyringe@protonmail.com>
2015-12-12 23:53:15 +0330

    Set Jade as default templating engine.

--- config/express.js
+++ config/express.js
@@ -20,6 +20,9 @@
     app.use(bodyParser.json());
     app.use(methodOverride());
 
+    app.set('views', '../app/views');
+    app.set('view engine', 'jade');
+
     require('../app/routes/index.server.routes')(app);
     return app;
  };
