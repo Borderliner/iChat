c987a72f9f28c2861be5d0cc85558831a8cdfc6d
Psyringe <psyringe@protonmail.com>
2016-01-22 14:24:54 +0330

    Added website field for users

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -2,16 +2,43 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    username: String,
+    firstName: {
+        type: String,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true
+    },
+    username: {
+        type: String,
+        trim: true
+    },
     password: String,
-    created:{
+    website: {
+        type: String,
+        get: function(url){
+            if(!url){
+                return url;
+            }
+            else {
+                if(url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0){
+                    url = 'http://' + url;
+                }
+                return url;
+            }
+        }
+    },
+    created: {
         type: Date,
         default: Date.now
     },
     updated: Date
 });
+UserSchema.set('toJSON', {getters: true});
 
 mongoose.model('User', UserSchema);
