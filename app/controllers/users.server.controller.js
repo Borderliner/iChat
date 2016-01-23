b63e6015503f6de3018dbfb8883ca9141d2e72d3
Psyringe <psyringe@protonmail.com>
2016-01-23 23:52:30 +0330

    Added signup/signin methods to User controller

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,5 +1,79 @@
 //Requirements
-var User = require('mongoose').model('User');
+var User = require('mongoose').model('User'),
+    passport = require('passport');
+
+var getErrorMessage = function(err){
+    var message = '';
+
+    if(err.code){
+        switch(err.code){
+            case 11000:
+            case 11001:
+                message = 'Username already exists';
+                break;
+            default:
+                message = 'Something went wrong. Report the issue please';
+                break;
+        }
+    } else {
+        for(var errName in err.errors){
+            if(err.errors[errName].message){
+                message = err.errors[errName].message;
+            }
+        }
+    }
+
+    return message;
+};
+
+exports.renderSignIn = function(req, res, next){
+    if(!req.user){
+        res.render('singin', {
+            title: 'Sign-In Form',
+            messages: req.flash('error') || req.flash('info')
+        });
+    } else {
+        return res.redirect('/');
+    }
+};
+
+exports.renderSignUp = function(req, res, next){
+    if(!req.user){
+        res.render('signup', {
+            title: 'Sign-Up Form',
+            messages: req.flash('error')
+        });
+    }
+};
+
+exports.signup = function(req, res, next){
+    if(!req.user){
+        var user = new User(req.body);
+        var message = null;
+
+        user.provider = 'local';
+        user.save(function(err){
+            if(err){
+                message = getErrorMessage(err);
+                req.flash('error', message);
+                return res.redirect('/signup');
+            }
+            req.login(user, function(err){
+                if(err){
+                    return next(err);
+                }
+                return res.redirect('/');
+            });
+        });
+    } else {
+        return res.redirect('/');
+    }
+};
+
+exports.signout = function(req, res){
+    req.logout();
+    res.redirect('/');
+};
 
 //Create new users
 exports.create = function(req, res, next){
