2b102708080058d28a7c552b8d34a912ab16cdf1
Psyringe <psyringe@protonmail.com>
2016-01-23 22:44:24 +0330

    Commentized(!) the express.js and server.js files

--- config/express.js
+++ config/express.js
@@ -1,3 +1,4 @@
+//Requirements
 var config = require('./config'),
     express = require('express'),
     morgan = require('morgan'),
@@ -5,38 +6,52 @@ var config = require('./config'),
     compress = require('compression'),
     methodOverride = require('method-override'),
     session = require('express-session'),
-    var passport = require('passport');
+    passport = require('passport');
 
+/* This file, modifies the express() object, and passes it along to the
+ * server.js file. It basically adds middlewares along the requests and
+ * responses, and sets the template engine.
+ */
 module.exports = function(){
     var app = express();
 
+    //Formats the requests and logs them on output
     if(process.env.NODE_ENV === 'development'){
         app.use(morgan('dev'));
     }
+    //Compresses data
     else if(process.env.NODE_ENV === 'production'){
         app.use(compress());
     }
 
+    //Parses body to and converts it to JSON
     app.use(bodyParser.urlencoded({
         extended: true
     }));
     app.use(bodyParser.json());
     app.use(methodOverride());
+    //Express-session initialization
     app.use(session({
         saveUninitialized: true,
         resave: true,
         secret: config.sessionSecret
     }));
 
-
+    //Sets /app/views as the default folder for view files
     app.set('views', './app/views');
+    //Sets the templating engine to Jade
     app.set('view engine', 'jade');
 
+    //Initializes passport for authentication
     app.use(passport.initialize());
     app.use(passport.session());
 
+    //Registers routes to express
     require('../app/routes/index.server.routes')(app);
     require('../app/routes/users.server.routes')(app);
+    //Sets public folder to /public, which holds client-side files
     app.use(express.static('./public'));
+
+    //Returns the modified object
     return app;
 };
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+//Set the NODE_ENV environment variable to 'development', IF NOT SET BY OS
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+//Requirements
 var database = require('./config/database'),
     express = require('./config/express'),
     passport = require('./config/passport');
 
-
+//Initialization
 var db = database();
 var app = express();
 var passport = passport();
 
+/* Set the application port to the first argument
+ *    Example: node server.js 3000
+ *    This will run the app on port 3000
+ */
 var port = process.argv[2];
 app.listen(port);
 console.log('Server is running. Listening to port ' + port);
