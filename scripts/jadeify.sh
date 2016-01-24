7c8e64d66e64ef1808e8006c7d2f9ce9b9877b62
Psyringe <psyringe@protonmail.com>
2016-01-25 00:43:53 +0330

    Added startup script, so it can compile jade files before server start. Start the server using 'npm start' command.

--- scripts/jadeify.sh
+++ scripts/jadeify.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 echo "Compiling Jade files in public folder"
 
-for i in `find ../public/ -name *.jade` ;
+for i in `find public/ -name *.jade` ;
 	do jade $i ; 
 done
---
+++ scripts/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+npm run jadeify
+node server $1
