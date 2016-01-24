d9ea1afe0af86fb74a255309388d4c1ed34b5daa
Psyringe <psyringe@protonmail.com>
2016-01-25 01:57:12 +0330

    Added ngRoute module, and routing to root URL

--- public/application.js
+++ public/application.js
@@ -1,5 +1,9 @@
 var mainApplicationModuleName = 'mean';
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
+
+mainApplicationModule.config(['$locationProvider', function($locationProvider){
+    $locationProvider.hashPrefix('!');
+}]);
 
 angular.element(document).ready(function(){
 	angular.bootstrap(document, [mainApplicationModuleName]);
