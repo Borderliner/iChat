a83fb7f7d522d95a30c41e2f5546ce1a82ab2f7f
Psyringe <psyringe@protonmail.com>
2016-01-30 22:37:36 +0330

    Added Authentication service to index.html

--- public/application.js
+++ public/application.js
@@ -1,9 +1,16 @@
 var mainApplicationModuleName = 'mean';
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, [
+	'ngRoute',
+	'example',
+	'users'
+]);
 
 mainApplicationModule.config(['$locationProvider', function($locationProvider){
     $locationProvider.hashPrefix('!');
 }]);
+
+if (window.location.hash === '#_=_')
+	window.location.hash = '#!';
 
 angular.element(document).ready(function(){
 	angular.bootstrap(document, [mainApplicationModuleName]);
