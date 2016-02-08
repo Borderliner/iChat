b496952d4e3184bcdc24a3d0333ed22a1d5cca84
Psyringe <psyringe@protonmail.com>
2016-02-08 22:03:35 +0330

    Created chat page, and it now sends user info from database to chat page.

--- app/controllers/chat.server.controller.js
+++ app/controllers/chat.server.controller.js
@@ -1,6 +1,8 @@
 exports.render = function(req, res){
+    req.session.user = req.user ? req.user : null;
     res.render('chat', {
         title: 'iChat Main Room',
-        userObject: req.user
+        userObject: req.user,
+        userUsername: req.session.user ? req.session.user.username : ''
     });
 };
