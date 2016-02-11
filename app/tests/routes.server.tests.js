34f93fa5b5c806e7f902d7869c0d3241de4b5ebd
Psyringe <psyringe@protonmail.com>
2016-02-11 22:31:48 +0330

    Added test suite for routings and static pages

---
+++ app/tests/routes.server.tests.js
@@ -0,0 +1,57 @@
+var app = require('../../server'),
+    should = require('should'),
+    request = require('supertest');
+
+describe('Routing Unit Tests:', function(){
+    describe('Testing Static Pages:', function(){
+        it('should retrieve homepage', function(done){
+            request(app).get('/')
+                .expect(200)
+                .expect('Content-Type', '/html/')
+                .end(function(err, res){
+                    res.status.should.be.exactly(200);
+                    done();
+                });
+        });
+
+        it('should retrieve signup page', function(done){
+            request(app).get('/signup')
+                .expect(200)
+                .expect('Content-Type', '/html/')
+                .end(function(err, res){
+                    res.status.should.be.exactly(200);
+                    done();
+                });
+        });
+
+        it('should retrieve signin page', function(done){
+            request(app).get('/signin')
+                .expect(200)
+                .expect('Content-Type', '/html/')
+                .end(function(err, res){
+                    res.status.should.be.exactly(200);
+                    done();
+                });
+        });
+
+        it('should retrieve about page', function(done){
+            request(app).get('/about')
+                .expect(200)
+                .expect('Content-Type', '/html/')
+                .end(function(err, res){
+                    res.status.should.be.exactly(200);
+                    done();
+                });
+        });
+
+        it('should retrieve chat page', function(done){
+            request(app).get('/chat')
+                .expect(200)
+                .expect('Content-Type', '/html/')
+                .end(function(err, res){
+                    res.status.should.be.exactly(200);
+                    done();
+                });
+        });
+    });
+});
